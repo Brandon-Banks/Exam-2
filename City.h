ba3d06b0b6bbcd1d6adcf0e1bfa44a0f7a856906
Brandon Banks <brandonjb907@outlook.com>
2017-04-20 21:58:22 -0700

    Update City.h

--- City.h
+++ City.h
@@ -1,58 +1,39 @@
 #ifndef CITY_H_
 #define CITY_H_
 
-#include <iostream>
-#include <vector>
 #include <string>
-#include <cmath>
+#include <vector>
 #include <iomanip>
+#include <iostream>
+
+#include "Citizen.h"
+
+using namespace std;
 
 class City
 {
-private:
-	string _cityName;
-
 public:
-
-	City();
-
-	//Creates a new city with the given name
-	//When the city is created you need to restore
-	//it's population from a file.
-	//Hint: You will want to make the file name
-	//be based on the name of the city.
 	City(string cityName);
 
-	//This is the destructor for the city.  When
-	//this city is destroyed, save the population of
-	//the city to a file so that you can restore
-	//it in the constructor the next time that
-	//a city with this name is created.
-	
-	City(City* c);
+	~City();
 
-	//Returns the city name
-	string getCityName();
+	int populationSize;
 
-	//Returns the number of citizens in this city
-	int populationSize();
+	string getCityName() const;
 
-	//Returns the citizen at the given index.
-	Citizen* getCitizenAtIndex(int index);
+	City* getCitizenAtIndex(int index);
 
-	//Adds a citizen to this city. You will need to
-	//make a copy of this citizen so that you make
-	//sure to keep it around for the life of the city.
 	void addCitizen(Citizen* citizen);
 
-	//Returns the citizen with the given id.
-	Citizen* getCitizenWithId(int id);
+	int size();
 
-	//Returns a vector of citizens that all have
-	//the given color as their favorite color.
-	//For example, if color is “Blue” this will return all citizens
-	//for this city who’s favorite color is Blue.
-	vector<Citizen*> getCitizensForFavoriteColor(string color);
+	Citizen* getCitizenWithID(int id);
+
+private:
+	string _cityName;
+	vector<Citizen*> citizenList;
 };
+
+
 
 #endif
